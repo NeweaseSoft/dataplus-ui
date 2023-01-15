d27e7ee5224a75c85861f6c036759116418add8a
fushuwei <331044761@qq.com>
2023-01-15 22:39:09 +0800

    提交本地代码, 未测试

---
+++ src/layouts/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <l-header
+    v-if="settingStore.showHeader"
+    :show-logo="settingStore.showHeaderLogo"
+    :theme="settingStore.displayMode"
+    :layout="settingStore.layout"
+    :is-fixed="settingStore.isHeaderFixed"
+    :menu="headerMenu"
+    :is-compact="settingStore.isSidebarCompact"
+  />
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
