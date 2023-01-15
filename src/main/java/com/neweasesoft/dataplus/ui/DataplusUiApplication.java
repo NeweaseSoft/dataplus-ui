a7e857db24aebfd98059d72a2f610f65d897d9f6
fushuwei <331044761@qq.com>
2023-01-15 23:41:52 +0800

    搭建前端开发框架

---
+++ src/main/java/com/neweasesoft/dataplus/ui/DataplusUiApplication.java
@@ -0,0 +1,13 @@
+package com.neweasesoft.dataplus.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataplusUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataplusUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neweasesoft/dataplus/ui/DataplusUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neweasesoft.dataplus.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataplusUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
